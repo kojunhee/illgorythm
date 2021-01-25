dafb29c60e87ac307fe118619ca3e44168f346ca
kojunhee <poo9705@naver.com>
2021-01-25 18:02:25 +0900

    candy not solved yet

---
+++ brute_force/3085_candy.py
@@ -0,0 +1,42 @@
+def MaxNum(a):
+    chain = 1
+    maxchain = 1
+    for i in range(len(a)-1):
+        if a[i] == a[i+1]:
+            chain += 1
+            if i+1 == len(a)-1:
+                if maxchain < chain:
+                    maxchain = chain
+        else:
+            if maxchain < chain:
+                maxchain = chain
+            chain = 1
+    return maxchain
+
+n = int(input())
+array = []
+for i in range(n):
+    string = list(input())
+    array.append(string)
+#print(array)
+#print(array[0][1])
+
+firstmax = 1
+secondmax =1
+for i in range(n):
+    for j in range(n-1):
+        (array[i][j],array[i][j+1])=(array[i][j+1],array[i][j])
+        temp = MaxNum(array(i))
+        if temp > firstmax:
+            firstmax = temp
+        (array[i][j+1],array[i][j])=(array[i][j],array[i][j+1])
+
+print(firstmax)
+
+'''array = list(map(list,zip(*array)))
+
+for i in range(n):
+    for j in range(n-1):
+        (array[j][i],array[j+1][i])=(array[j+1][i],array[j][i])
+        ManNum(array(i))
+        (array[j+1][i],array[j][i])=(array[j][i],array[j+1][i])'''
---
+++ brute_force/test.py
@@ -0,0 +1,19 @@
+
+def MaxNum(a):
+    chain = 1
+    maxchain = 1
+    for i in range(len(a)-1):
+        if a[i] == a[i+1]:
+            chain += 1
+            if i+1 == len(a)-1:
+                if maxchain < chain:
+                    maxchain = chain
+        else:
+            if maxchain < chain:
+                maxchain = chain
+            chain = 1
+    return maxchain
+
+array=[['y','y','y'],['e','j','j']]
+
+print(list(map(list,zip(*array))))
